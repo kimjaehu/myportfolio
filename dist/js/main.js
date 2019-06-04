dca9b6bbe885e202ac35769c183696415795c31f
Jay Kim <kimjaehu@gmail.com>
2019-06-04 15:29:11 -0400

    projects page

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,6 +9,7 @@ const navItems = document.querySelectorAll('.nav-item');
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
+navItems.forEach(item => item.addEventListener('click', closeMenu));
 
 function toggleMenu() {
   if (!showMenu) {
@@ -30,4 +31,17 @@ function toggleMenu() {
     // Set Menu State
     showMenu = false;
   }
+}
+
+function closeMenu() {
+  menuBtn.classList.remove('close');
+  menu.classList.remove('show');
+  menuNav.classList.remove('show');
+  menuBranding.classList.remove('show');
+  navItems.forEach(item => {
+    item.classList.remove('show');
+  });
+
+  // Set Menu State
+  showMenu = false;
 }
